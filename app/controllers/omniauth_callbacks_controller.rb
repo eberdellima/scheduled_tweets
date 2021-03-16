61fb095a8a839cdaefa76025c1f0a013a89ff1d8
Bhfreagra <eberdellima@outlook.com>
2021-03-16 13:19:55 +0100

    finish implementing twitter accounts connections

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -3,6 +3,23 @@
 # OmniauthCallbacksController documentation
 class OmniauthCallbacksController < ApplicationController
   def twitter
-    render plain: 'success'
+    auth_info = auth.info
+    auth_credentials = auth.credentials
+
+    twitter_account = Current.user.twitter_accounts.where(username: auth_info.nickname).first_or_initialize
+    twitter_account.update(
+      name: auth_info.name,
+      image: auth_info.image,
+      token: auth_credentials.token,
+      secret: auth_credentials.secret
+    )
+
+    redirect_to root_path, notice: 'Successfully connected your account!'
+  end
+
+  private
+
+  def auth
+    request.env['omniauth.auth']
   end
 end
